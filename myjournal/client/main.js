85866fad45171bd8875067590b7136d57f18387a
Sean Lee <lee.weihan.sean@gmail.com>
2020-08-24 13:05:50 +0800

    Add entries feature

--- myjournal/client/main.js
+++ myjournal/client/main.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 
 import App from '../imports/ui/App.js';
+import AddEntry from '../imports/ui/AddEntry.js'
 
 Meteor.startup(() => {
-  ReactDOM.render(<App />, document.getElementById('app'));
+  ReactDOM.render(<AddEntry />, document.getElementById('app'));
 });
